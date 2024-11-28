9b4c2abab245b19e0dc3ccb847c15acb1123965c
Blanca <blancacmaestre@gmail.com>
2024-11-28 18:06:12 +0100

    new plots

--- pyBB_simulate_galaxies.py
+++ pyBB_simulate_galaxies.py
@@ -12,7 +12,7 @@ xysize, vsize = 75, 62        # Number of pixels/channels   #Value in model1 = 5
 pixsize   = 32                  # Size of pixels (arcsec)     #Value in model1 = 20
 chwidth   = -5.12                   # Channel width (km/s)        #Value in model1 = 5
 beamFWHM  = 180                  # Beam size (arcsec)          #Value in model1 = 60
-modname   = 'modelwith0.0015rms_othervdisp'            # Name of the model           
+modname   = 'model4'            # Name of the model           
 noiserms  = 0.0015              # RMS noise in Jy/beam        #Value in model1 = 0.01
 
 # This the main BBarolo executable
@@ -22,18 +22,21 @@ ringfile = "/Users/blanca/Documents/TESIS/software/Bbarolo-1.7/output/ngc2403/ri
 t = Table.read(ringfile,format='ascii')
 
 
-
-
 # Basic parameters of the model
 radmax  = 1250                        #Value in model1 = 240
 radii   = np.arange(0,radmax,pixsize)
-print(len(radii))
-dens    = np.full(len(radii),1.) #50*np.exp(-radii/400-100/(0.5*radii+100))
-vrot    = t['VROT(km/s)']#2./np.pi*150*np.arctan(radii/30.)                     #2./np.pi*200.*np.arctan(radii/10.)
 
-print(vrot)
-vdisp   = np.linspace(30, 10, len(radii)).tolist()
-#vdisp   = np.full(len(radii),20.)         #Value in model1 = 10
+dens    = np.full(len(radii),1.) 
+#dens   = 50*np.exp(-radii/400-100/(0.5*radii+100))
+dens_shape = "constant, value 1"
+
+vrot   = t['VROT(km/s)']
+#vrot    = 2./np.pi*150*np.arctan(radii/30.)
+vrot_shape = "normal, taken from values of what Bbarlo got with ngc2403"
+
+#vdisp   = np.linspace(30, 10, len(radii)).tolist()
+vdisp   = np.full(len(radii),20.)         #Value in model1 = 10
+vdisp_shape = "linearly decreasing from 30 to 10"
 pa      = np.full(len(radii),124.147)       #Value in model1 = 30 # actual angle of ngc2405 is 123.7
 inc     = np.full(len(radii),50.988)         #Value in model1 = 60
 z0      = np.full(len(radii),0.)         #Value in model1 = 30
@@ -50,11 +53,13 @@ vsys    = np.full(len(radii),134.109)       #Value in model1 = 30
 #s = SimulatedGalaxyCube(axisDim=[xysize, xysize, vsize],\
 #                        cdelts=[-pixsize/3600., pixsize/3600., chwidth],\
 #                        beam=beamFWHM/3600., bunit='JY/BEAM', obj=modname)
+crpixs=[38.,38., 1]
+crvals=[114.22, 65.58, 289.61]
 
 s = SimulatedGalaxyCube(axisDim=[xysize, xysize, vsize],\
                         cdelts=[-pixsize/3600., pixsize/3600., chwidth],\
-                        crpixs=[38.,38., 1],\
-                        crvals=[114.22, 65.58, 289.61],\
+                        crpixs=crpixs,\
+                        crvals=crvals,\
                         beam=beamFWHM/3600., bunit='JY/BEAM', obj=modname)
 
 # Setting up galaxy parameters (if parameters are not given, they are random!)
@@ -64,17 +69,56 @@ print(s)
 
 # Running BB and creating a model
 print (f"Simulating {modname}...",flush=True,end='')
-s.run(exe=BBmain,stdout='null',smooth=True,noise=noiserms)
+s.run(exe=BBmain,stdout='null',smooth=True,noise=noiserms) #HERE IS WHERE THE NOISE IS ADDED
 print (f"Done! ")
 
 # Cleaning working directory 
-if not os.path.isdir('models'): os.mkdir('models')
+if not os.path.isdir(f'models'): os.mkdir(f'models')
 mf = f'{modname}mod_noise.fits' if noiserms>0 else f'{modname}mod_nonorm.fits'
 subprocess.call([f'{BBmain}','--modhead',f'{modname}/{mf}','OBJECT',f'{modname}'],stdout=subprocess.DEVNULL)
 subprocess.call([f'mv',f'{modname}/{mf}',f'models/{modname}.fits'],stdout=subprocess.DEVNULL)
 subprocess.call([f'mv',f'{modname}/{modname}_params.txt',f'models/'],stdout=subprocess.DEVNULL)
 subprocess.call([f'rm','-rf',f'{modname}','emptycube.fits','galaxy_params.txt'],stdout=subprocess.DEVNULL)
 
+
 print (f"Model written in models/ directory. See you soon!")
+ 
+
+with open(os.path.join(f"models", f"{modname}_input.txt"), 'w') as file:
+    file.write(f"Name of the model = {modname}\n")
+    file.write("\n")
+    file.write("CUBE PARAMETERS\n")
+    file.write("\n")
+    file.write(f"Number of pixels in x/y = {xysize}\n")
+    file.write(f"Size of pixels (arcsec)= {xysize}\n")
+    file.write(f"Number of channels = {xysize}\n")
+    file.write(f"Channel width = {chwidth}\n")
+    file.write(f"Beam width = {beamFWHM}\n")
+    file.write(f"RMS noise in Jy/beam = {xysize}\n")
+    file.write(f"crvals = {crvals}\n")
+    file.write(f"crpixs = {crpixs}\n")
+    file.write("\n")
+    file.write("GALAXY PARAMETERS\n")
+    file.write("\n")
+    file.write(f"Maximum radius of galaxy = {radmax}\n")
+    file.write(f"Beam width = {beamFWHM}\n")
+    file.write(f"Number of radii = {len(radii)}\n")
+    file.write(f"Density profile initial value = {dens[0]}\n")
+    file.write(f"Density ptofile shape = {dens_shape}\n")
+    file.write("\n")
+    file.write("VELOCITIES\n")
+    file.write(f"Rotational velocity initial value = {vrot[0]}\n")
+    file.write(f"Rotational velocity shape = {vrot_shape}\n")
+    file.write(f"Velocity dispersion initial value= {vdisp[0]}\n")
+    file.write(f"Velocity dispersion shape = {vdisp_shape}\n")
+    file.write("\n")
+    file.write("GEOMETRY\n")
+    file.write(f"Position angle = {pa[0]}\n")
+    file.write(f"Inclination = {inc[0]}\n")
+    file.write(f"Thickness = {z0[0]}\n")
+    file.write(f"Systemic velocity (km/s) = {vsys[0]}\n")
+    
 
 
+
+print(f"{modname}_initial_params.txt created with default parameters.")
